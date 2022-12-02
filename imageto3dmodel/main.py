fb275a90a73701bb7312a6876dd23651590d8d4a
Kexiky <51199842+Kexiky@users.noreply.github.com>
2022-12-02 23:38:40 +0300

    Adding disparity

--- imageto3dmodel/main.py
+++ imageto3dmodel/main.py
@@ -1,5 +1,6 @@
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -28,5 +29,23 @@ if __name__ == '__main__':
     img_l = cv2.imread(path_img_L)
     img_r = cv2.imread(path_img_R)
 
-    pictureimshow([img_l, img_r], (1, 2), ['left', 'right'])
+    minDisparity = 0
+    numDisparities = 64
+    blockSize = 8
+    disp12MaxDiff = 1
+    uniquenessRatio = 10
+    speckleWindowSize = 10
+    speckleRange = 8
 
+    stereo = cv2.StereoSGBM_create(minDisparity=minDisparity,
+                                   numDisparities=numDisparities,
+                                   blockSize=blockSize,
+                                   disp12MaxDiff=disp12MaxDiff,
+                                   uniquenessRatio=uniquenessRatio,
+                                   speckleWindowSize=speckleWindowSize,
+                                   speckleRange=speckleRange
+                                   )
+
+    disp = stereo.compute(img_l, img_r).astype(np.float32)
+    disp = cv2.normalize(disp, 0, 255, cv2.NORM_MINMAX)
+    pictureimshow([img_l, img_r, disp], (1, 3), ['left', 'right', 'disparity'])
